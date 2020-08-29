8b75c3f0512bc9a3ec13974348eed57c80ccfb55
Maja Tengstedt <maja.tengstedt@gmail.com>
2020-08-29 11:15:30 +0200

    Initial commit

---
+++ src/HalloWorld.java
@@ -0,0 +1,11 @@
+public class HalloWorld {
+
+    public static void main (String args []){
+
+        System.out.println("Hallo World");
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
